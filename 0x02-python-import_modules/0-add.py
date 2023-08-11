99957bef43fcdf698e1fb5a397541ef7071ed020
Zoohdev <sesethu.ngqwaru752@gmail.com>
2023-08-11 03:41:49 -0700

    Import modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from add_0 import add #import function add from add_0.py file
+
+#assign integers to variables a and b
+a = 1
+b = 2
+
+#assign function add with previously defined integers
+result = add(a, b)
+print(f"{a} + {b} = {result}")
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+Python Import MOdules Tasks
